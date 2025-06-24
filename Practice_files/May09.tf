28aca87ad71c474cfaf295ac4d504e528221ff8e
Jayesh Hyalij <jhyalij940@gmail.com>
2025-06-24 19:23:50 +0530

    addpra

--- Practice_files/May09.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0e35ddab05955cf57"
-  instance_type = "t2.micro"
-  subnet_id     = "subnet-05cc8cadb7bec5687"
-
-  tags = {
-    Name = "HelloWorld"
-    }
-}
